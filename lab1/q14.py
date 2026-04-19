02e1e36d42a38b5c471da945cb014a8596026923
Louzadino <guilhermelouzada852@gmail.com>
2026-04-19 16:10:25 -0300

    Questão 14 lab 1 resolvida

---
+++ lab1/q14.py
@@ -0,0 +1,17 @@
+from q1_4 import head, tail
+
+def at_list(num, l):
+    if l:
+        if head(l) == num:
+            return True
+        else:
+            return at_list(num, tail(l))
+    else:
+        return False
+
+l = [1, 5, 212, 6, 5, 35, 6, 10, 29]
+num = int(input("Digite um número para saber se ele está na lista: "))
+if at_list(num, l):
+    print("Está na lista")
+else:
+    print("Não está na lista")
